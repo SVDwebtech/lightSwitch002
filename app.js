48aefb7ebf03dae0fc9f7e31474daf1e56e20bcb
Schalk van Dyk <schalkvandyk1979@gmail.com>
2021-10-14 15:21:19 +0200

    add darkRoomColorToggle functionality to app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const button = document.querySelector(".switch");
 const bulb = document.querySelector(".bulb");
+const room = document.querySelector(".room");
 
+room.classList.add("darkRoomColorToggle");
 button.addEventListener("click", () => {
 	bulb.classList.toggle("lightSwitchColorToggle");
+	room.classList.toggle("darkRoomColorToggle");
 });
