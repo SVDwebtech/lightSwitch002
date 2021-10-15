192d31649b29274bd9530b699c1711779aa5e429
Schalk van Dyk <schalkvandyk1979@gmail.com>
2021-10-15 17:02:07 +0200

    add behaviour functionality to wall switch in app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const button = document.querySelector(".switch");
 const bulb = document.querySelector(".bulb");
 const room = document.querySelector(".room");
+const switchOn = document.querySelector(".switchOn");
+const switchOff = document.querySelector(".switchOff");
 
 room.classList.add("darkRoomColorToggle");
 button.addEventListener("click", () => {
 	bulb.classList.toggle("lightSwitchColorToggle");
 	room.classList.toggle("darkRoomColorToggle");
+	switchOn.classList.toggle("lightSwitchToggleONBtnColor");
+	switchOff.classList.toggle("lightSwitchToggleOFFBtnColor");
+	switchOn.classList.toggle("switchOn-clip");
+	switchOff.classList.toggle("switchOff-clip");
 });
